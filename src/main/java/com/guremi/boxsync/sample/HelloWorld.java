c86c8f6c481047a3162e4890ea8e6c6911c6b158
htaka <taka@fullmarks.co.jp>
2014-12-08 10:31:20 +0900

    .

--- src/main/java/com/guremi/boxsync/sample/HelloWorld.java
+++ src/main/java/com/guremi/boxsync/sample/HelloWorld.java
@@ -15,15 +15,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HelloWorld {
-	private static final Logger LOG = LoggerFactory.getLogger(HelloWorld.class);
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorld.class);
 
     public static final int PORT = 4000;
 
     public static void main(String[] args) throws AuthFatalFailureException, BoxServerException, BoxRestException {
-		
-		Config config = ConfigFactory.load("application.json");
-		String key = config.getString("box.key");
-		String secret = config.getString("box.secret");
+
+        Config config = ConfigFactory.load("application.json");
+        String key = config.getString("box.key");
+        String secret = config.getString("box.secret");
 
         if (key.equals("YOUR API KEY HERE")) {
             System.out.println("Before this sample app will work, you will need to change the");
@@ -37,68 +37,62 @@ public class HelloWorld {
             Desktop.getDesktop().browse(java.net.URI.create(url));
             code = getCode();
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error(e.getMessage(), e);
         }
 
         BoxClient client = getAuthenticatedClient(code, key, secret);
 
-        BoxFolder boxFolder= client.getFoldersManager().getFolder("0",null);
+        BoxFolder boxFolder = client.getFoldersManager().getFolder("0", null);
         ArrayList<BoxTypedObject> folderEntries = boxFolder.getItemCollection().getEntries();
         int folderSize = folderEntries.size();
-        for (int i = 0; i <= folderSize-1; i++){
+        for (int i = 0; i <= folderSize - 1; i++) {
             BoxTypedObject folderEntry = folderEntries.get(i);
             String name = (folderEntry instanceof BoxItem) ? ((BoxItem)folderEntry).getName() : "(unknown)";
             System.out.println("i:" + i + ", Type:" + folderEntry.getType() + ", Id:" + folderEntry.getId() + ", Name:" + name);
         }
     }
 
-    private static BoxClient getAuthenticatedClient(String code, String key, String secret) throws BoxRestException,     BoxServerException, AuthFatalFailureException {
-        BoxClient client = new BoxClient(key, secret, null);
-        BoxOAuthToken bt =  client.getOAuthManager().createOAuth(code, key, secret, "http://localhost:" + PORT);
+    private static BoxClient getAuthenticatedClient(String code, String key, String secret) throws BoxRestException, BoxServerException, AuthFatalFailureException {
+        BoxClient client = new BoxClient(key, secret, null, null, null);
+        BoxOAuthToken bt = client.getOAuthManager().createOAuth(code, key, secret, "http://localhost:" + PORT);
         client.authenticate(bt);
         return client;
     }
-
 
     private static String getCode() throws IOException {
 
         ServerSocket serverSocket = new ServerSocket(PORT);
         Socket socket = serverSocket.accept();
-        BufferedReader in = new BufferedReader (new InputStreamReader (socket.getInputStream ()));
-        while (true)
-        {
-            String code = "";
-            try
-            {
-                BufferedWriter out = new BufferedWriter (new OutputStreamWriter (socket.getOutputStream ()));
-                out.write("HTTP/1.1 200 OK\r\n");
-                out.write("Content-Type: text/html\r\n");
-                out.write("\r\n");
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        while (true) {
+            String code;
+            try {
+                try (BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+                    out.write("HTTP/1.1 200 OK\r\n");
+                    out.write("Content-Type: text/html\r\n");
+                    out.write("\r\n");
 
-                code = in.readLine ();
-                System.out.println (code);
-                String match = "code";
-                int loc = code.indexOf(match);
+                    code = in.readLine();
+                    System.out.println(code);
+                    String match = "code";
+                    int loc = code.indexOf(match);
 
-                if( loc >0 ) {
-                    int httpstr = code.indexOf("HTTP")-1;
-                    code = code.substring(code.indexOf(match), httpstr);
-                    String parts[] = code.split("=");
-                    code=parts[1];
-                    out.write("Now return to command line to see the output of the HelloWorld sample app.");
-                } else {
-                    // It doesn't have a code
-                    out.write("Code not found in the URL!");
+                    if (loc > 0) {
+                        int httpstr = code.indexOf("HTTP") - 1;
+                        code = code.substring(code.indexOf(match), httpstr);
+                        String parts[] = code.split("=");
+                        code = parts[1];
+                        out.write("Now return to command line to see the output of the HelloWorld sample app.");
+                    } else {
+                        // It doesn't have a code
+                        out.write("Code not found in the URL!");
+                    }
                 }
 
-                out.close();
-
                 return code;
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 //error ("System: " + "Connection to server lost!");
-                System.exit (1);
+                System.exit(1);
                 break;
             }
         }
