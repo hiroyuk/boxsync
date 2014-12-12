36fdf053081cb5f73260c4c7080b73b5d4200395
htaka <taka@fullmarks.co.jp>
2014-12-12 15:34:34 +0900

    google oauthからコピー

---
+++ src/main/java/com/guremi/boxsync/oauth2/LocalServer.java
@@ -0,0 +1,125 @@
+package com.guremi.boxsync.oauth2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class LocalServer {
+    private Server server;
+    private String code;
+    private String error;
+    private final Lock lock = new ReentrantLock();
+    private final Condition authoriseResponse = lock.newCondition();
+    private static final String CALLBACK_PATH = "/callback";
+
+    private int port;
+    private String host;
+
+    public LocalServer() {
+        this("localhost", -1);
+    }
+
+    public LocalServer(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getRegirectUri() throws Exception {
+        if (port == -1) {
+            port = getUnusedPort();
+        }
+        server = new Server();
+        ServerConnector sc = new ServerConnector(server);
+        sc.setHost(host);
+        sc.setPort(port);
+        server.addConnector(sc);
+
+        server.start();
+        return "http://" + host + ":" + port + CALLBACK_PATH;
+    }
+
+    public String waitForCode() throws IOException {
+        lock.lock();
+        try {
+            while(code == null || error == null) {
+                authoriseResponse.awaitUninterruptibly();
+            }
+            if (error != null) {
+                throw new IOException("User authorization failed.(" + error + ")");
+            }
+            return code;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void stop() throws Exception {
+        if (server != null) {
+            server.stop();
+            server = null;
+        }
+    }
+
+    private int getUnusedPort() throws IOException {
+        Socket s = new Socket();
+        s.bind(null);
+        try {
+            return s.getLocalPort();
+        } finally {
+            s.close();
+        }
+    }
+
+    public int getPort() {
+        return ((ServerConnector)server.getConnectors()[0]).getLocalPort();
+    }
+
+    class CallbackHander extends AbstractHandler {
+        @Override
+        public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+            if (!CALLBACK_PATH.equals(target)) {
+                return;
+            }
+            writeResponseHtml(response);
+            response.flushBuffer();
+            ((Request)request).setHandled(true);
+            lock.lock();
+            try {
+                error = request.getParameter("error");
+                code = request.getParameter("code");
+                authoriseResponse.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        private void writeResponseHtml(HttpServletResponse response) throws IOException {
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType("text/html");
+
+            PrintWriter doc = response.getWriter();
+            doc.println("<html>");
+            doc.println("<head><title>OAuth 2.0 Authentication Token Recieved</title></head>");
+            doc.println("<body>");
+            doc.println("Received verification code. Closing...");
+            doc.println("<script type='text/javascript'>");
+            doc.println("window.setTimeout(function() {");
+            doc.println("    window.open('', '_self', ''); window.close(); }, 1000);");
+            doc.println("if (window.opener) { window.opener.checkToken(); }");
+            doc.println("</script>");
+            doc.println("</body>");
+            doc.println("</HTML>");
+            doc.flush();
+        }
+    }
+}
