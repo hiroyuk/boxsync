cc1a3bebb1b75fd0636ca795bf5568147d57f29a
Hiroyuki Taka <htaka@kuro.internal.guremi.com>
2014-12-17 12:47:55 +0900

    .

---
+++ src/main/java/com/guremi/boxsync/AllScaner.java
@@ -0,0 +1,38 @@
+package com.guremi.boxsync;
+
+import com.guremi.boxsync.utils.DigestUtils;
+import com.typesafe.config.ConfigObject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author htaka
+ */
+public class AllScaner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AllScaner.class);
+
+    public void scan() {
+        ConfigObject co = App.config.getObject("syncpair");
+        co.forEach((k, v) -> {
+            try {
+                Path path = Paths.get(k);
+                Files.walk(path)
+                        .parallel()
+                        .filter(p -> Files.isRegularFile(p))
+                        .forEach(p -> {
+                            LOG.info("starting scan, file: {}", p.toString());
+                            String digest = DigestUtils.getDigest(p);
+                        });
+            } catch (IOException ex) {
+                LOG.error(ex.getMessage(), ex);
+            }
+        });
+
+    }
+}
