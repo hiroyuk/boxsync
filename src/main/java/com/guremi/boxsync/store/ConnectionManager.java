d41a97f8f6335d56d7510c8b985af01c677d2064
Hiroyuki Taka <htaka@kuro.internal.guremi.com>
2014-12-17 12:51:41 +0900

    .

--- src/main/java/com/guremi/boxsync/store/ConnectionManager.java
+++ src/main/java/com/guremi/boxsync/store/ConnectionManager.java
@@ -1,19 +1,30 @@
 package com.guremi.boxsync.store;
 
-import com.guremi.boxsync.FileWatcher;
+import com.guremi.boxsync.App;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author htaka
  */
 public class ConnectionManager {
+    private static final Logger LOG = LoggerFactory.getLogger(ConnectionManager.class);
 	
 	public static Connection getConnection() throws SQLException {
-		String datafile = FileWatcher.config.getString("box.storepath") + "/sqlite.db";
+		String datafile = App.config.getString("box.storepath") + "/sqlite.db";
+        try {
+            Files.createDirectories(Paths.get(datafile).getParent());
+        } catch (IOException ex) {
+            LOG.error(ex.getMessage(), ex);
+        }
 		Connection connection = DriverManager.getConnection("jdbc:sqlite:" + datafile);
 
 		try (Statement stmt = connection.createStatement()) {
